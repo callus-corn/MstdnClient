6070eb7692ba9fa4a084719d9b143a6267c0a662
Yamazaki Mitsufumi <mitsufumi.yamazaki@outlook.jp>
2017-05-02 23:59:30 +0900

    add Activities
    Add autho and start activity.
    Change launche activity to start activity

---
+++ app/src/main/java/com/corn/collus/mstdnclient/activities/StartActivity.java
@@ -0,0 +1,15 @@
+package com.corn.collus.mstdnclient.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.corn.collus.mstdnclient.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
